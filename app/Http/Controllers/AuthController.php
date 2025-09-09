c7b249feb9a57d7bd2a4b97b6de01beda3867305
ReberteMartins <63303430+ReberteMartins@users.noreply.github.com>
2025-09-09 10:05:12 -0300

    :sparkles: feat(Auth): Implementa controle de autenticação com JWT, incluindo login, logout, refresh e recuperação de usuário

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    //
+    public function login(Request $request){
+        $credenciais = $request->only(['email', 'password']);
+
+        if (!$token = Auth::guard('api')->attempt($credenciais)) {
+            return response()->json(['error' => 'Credenciais inválidas'], 401);
+        }
+
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function logout(){
+        Auth::guard('api')->logout();
+        return response()->json(['msg' => 'Logout foi realizado com sucesso']);
+    }
+
+    public function refresh(){
+        $token = Auth::guard('api')->refresh();
+        return response()->json(['token' => $token]);
+    }
+
+    public function me(){
+        return response()->json(auth()->user());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CarroController;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\LocacaoController;
@@ -12,8 +13,19 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('cliente', ClienteController::class);
-Route::apiResource('carro', CarroController::class);
-Route::apiResource('locacao', LocacaoController::class);
-Route::apiResource('marca', MarcaController::class);
-Route::apiResource('modelo', ModeloController::class); 
+Route::group([
+    'middleware' => 'auth:api',
+], function($router) {
+    Route::apiResource('cliente', ClienteController::class);
+    Route::apiResource('carro', CarroController::class);
+    Route::apiResource('locacao', LocacaoController::class);
+    Route::apiResource('marca', MarcaController::class);
+    Route::apiResource('modelo', ModeloController::class);
+
+    Route::post('me', [AuthController::class, 'me']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+
+Route::post('login', [AuthController::class, 'login']);
+
